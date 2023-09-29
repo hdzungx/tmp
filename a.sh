9333a374813a199ac86ea56d5854556ff2dbd0c0
HDzungx <118291317+hdzungx@users.noreply.github.com>
2023-09-29 15:51:27 +0700

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,8 @@
+yay -Syu hyprland-git \
+    waybar-hyprland-git \
+    swaylock-effects \
+    wlogout \
+    wofi \
+    capitaine-cursors \
+    sweet-theme-git \
+    xdg-desktop-portal-hyprland \
